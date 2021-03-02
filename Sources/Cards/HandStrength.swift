19a2e1a3b0c6108087ab2a2710355c7418751670
Will Chiong <will@infomofo.com>
2021-03-02 13:55:26 -0500

    cards

---
+++ Sources/Cards/HandStrength.swift
@@ -0,0 +1,12 @@
+//
+//  HandStrength.swift
+//  Video Poker WatchKit Extension
+//
+//  Created by Will C on 10/11/20.
+//
+
+import Foundation
+    
+public enum HandStrength: Int {
+    case EmptyHand, HighCard, Pair, TwoPair, ThreeOfAKind, Straight, Flush, FullHouse, FourOfAKind, StraightFlush
+}
